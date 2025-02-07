c8f23d9d1b69f757febed030755ed8bcb52a928a
CodingCoderdk <dhakshinesh5771@gmail.com>
2025-02-07 23:56:56 +0530

    second commit

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { Editor } from '@/components/editor/Editor'
+import React from 'react'
+// import { Html } from '@/components/ui/html'
+
+const Document = () => {
+  return (
+    // <Html>
+    <div>
+        
+        <Editor />
+    </div>
+   
+  )
+}
+
+export default Document
