f240ed96d87a6a332672c3ac83fec49b165fd28a
CodingCoderdk <dhakshinesh5771@gmail.com>
2025-03-01 01:00:30 +0530

    user-mention before sentry

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 // import { User } from "@liveblocks/node";
 
 export const getClerkUsers = async ({userIds}: {userIds: string[]})=>{
@@ -26,5 +27,23 @@ export const getClerkUsers = async ({userIds}: {userIds: string[]})=>{
         }
         catch(error){
             console.log(`error while getting clerk users: ${error}`);
+        }
+}
+
+export const getDocumentUsers = async ({roomId, currentUser, text}: {roomId: string, currentUser: string, text: string}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+        const users = Object.keys(room.usersAccesses).filter((email)=> email !== currentUser);
+
+        if(text.length)
+{
+    const lowercaseText = text.toLowerCase();
+    const filteredUsers = users.filter((email:string)=> email.toLowerCase().includes(lowercaseText));
+
+    return parseStringify(filteredUsers);
+}
+    return parseStringify(users);
+}catch(error){
+            console.log(`error while getting document users: ${error}`);
         }
 }
